109a3ada27c581d3c4a6c4238c663aaaaee69b4f
Winston <nullruto@protonmail.ch>
2017-12-09 13:16:58 -0500

    Updated Protocol.java

--- Server/Protocol.java
+++ Server/Protocol.java
@@ -4,4 +4,6 @@ class Protocol {
 	public static final int JOIN_LOBBY = 2;
 	public static final int MOVE_TURN = 3;
 	public static final int MESSAGE = 4;
+	public static final int TURN = 5;
+	public static final int SCORE = 6;
 }
